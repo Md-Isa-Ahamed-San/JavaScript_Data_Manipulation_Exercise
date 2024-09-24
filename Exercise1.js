51bb3fbc6d68bd08338ecedb8aed340f885540f9
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2024-09-24 20:43:33 +0600

    first commit

---
+++ Exercise1.js
@@ -0,0 +1,18 @@
+const items = [
+  { categoryId: "A", images: ["img1.jpg", "img2.jpg"] },
+  { categoryId: "B", images: ["img3.jpg"] },
+  { categoryId: "A", images: ["img2.jpg", "img4.jpg"] },
+  { categoryId: "C", images: ["img5.jpg", "img6.jpg"] },
+  { categoryId: "B", images: ["img7.jpg", "img3.jpg"] },
+];
+const filteredObj = items.reduce((acc, item) => {
+  if (acc[item.categoryId]) {
+    acc[item.categoryId] = [
+      ...new Set([...acc[item.categoryId], ...item.images]),
+    ];
+  } else {
+    acc[item.categoryId] = [...item.images];
+  }
+  return acc;
+}, {});
+console.log(filteredObj);
